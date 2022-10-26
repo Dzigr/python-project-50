40f8c91792842a24337524a5b8036bab9c2de114
Dzigr <dmbrag@gmail.com>
2022-10-26 18:40:45 +0300

    Update code
    Create Makefile, added format in cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,5 +7,10 @@ def parse():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f",
+                        "--format",
+                        metavar="FORMAT",
+                        help="set format of output"
+                        )
 
     return parser.parse_args()
