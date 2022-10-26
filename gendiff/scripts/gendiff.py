6dd0b6c57b491c45862b48595728981ee47b9cc7
Dzigr <dmbrag@gmail.com>
2022-10-26 18:11:13 +0300

    Initiate project. 1st step complete

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+"""Main module."""
+# !/usr/bin/env python3
+
+from gendiff.cli import parse
+
+
+def main():
+    """Request fot two files"""
+    args = parse()
+
+
+if __name__ == "__main__":
+    main()
